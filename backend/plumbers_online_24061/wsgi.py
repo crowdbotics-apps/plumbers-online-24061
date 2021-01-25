f60f823b80360247d757295ebe3ce05dfb542c3a
Crowdbotics <team@crowdbotics.com>
2021-01-25 18:43:17 +0000

    Update models

--- backend/plumbers_online_24061/wsgi.py
+++ backend/plumbers_online_24061/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plumbers_online_24061.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plumbers_online_24061.settings")
 
 application = get_wsgi_application()
